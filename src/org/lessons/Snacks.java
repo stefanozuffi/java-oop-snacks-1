59716730ac405d34757dced255436985d3e74037
stefanozuffi <stefano.zuffi06@outlook.com>
2025-12-03 22:21:02 +0100

    snack 1

---
+++ src/org/lessons/Snacks.java
@@ -0,0 +1,11 @@
+package org.lessons;
+
+public class Snacks {
+
+    public static void main(String[] args) {
+        
+        Studente student_1 = new Studente("Franco", "Riva", 21);
+
+        System.out.println(student_1.getAll());
+    }
+}
---
+++ src/org/lessons/Studente.java
@@ -0,0 +1,17 @@
+package org.lessons;
+
+public class Studente {
+    private String name;
+    private String surname;
+    private int age;
+
+    public Studente(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public String getAll() {
+        return name + " " + surname + ", " + age + " years old";
+    }
+}
